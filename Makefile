daa88e55edee83fa37b138211e865f8feb8576a7
Arnaud Sautaux <arnaud.sautaux@mc-monitoring.com>
2024-07-05 13:49:54 +0200

    cannot be closed

--- Makefile
+++ Makefile
@@ -1,3 +1,9 @@
 
-main.out:
+main.out: main.c
 	gcc main.c -lglfw -o main.out
+
+clean:
+	rm -f *.out
+
+.PHONY:
+	clean
--- main.c
+++ main.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
+#ifdef WIN32
+#include <glad/gl.h>
+#endif
+
+void error_callback(int error, const char *description) {
+  fprintf(stderr, "Error: %s\n", description);
+}
 
 int main() {
   printf("Hello glfw\n");
+
+  glfwSetErrorCallback(error_callback);
 
   if (!glfwInit()) {
     printf("Init failed\n");
@@ -11,4 +20,20 @@ int main() {
   }
 
   printf("init ok\n");
+
+  GLFWwindow *window =
+      glfwCreateWindow(640, 480, "My Window Title", NULL, NULL);
+  if (!window) {
+    printf("Window creation failed\n");
+    goto terminate;
+  }
+  glfwMakeContextCurrent(window);
+
+  while (!glfwWindowShouldClose(window)) {
+  }
+
+  glfwDestroyWindow(window);
+terminate:
+  glfwTerminate();
+  printf("glfw terminated\n");
 }
